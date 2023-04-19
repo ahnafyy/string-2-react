619897753c7d8656ac8ec6505eb6b962d3e99b1d
ahnafyy <yasin011@umn.edu>
2023-04-19 13:18:04 -0500

    Add tests and attempt handling self closing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const { parseHTML } = require('linkedom');
 
 function convertNodeToReact(node) {
   if (node.nodeType === Node.ELEMENT_NODE) {
+    // Get the tag name
+    const tagName = node.tagName.toLowerCase();
+
     // Convert the attributes of the element to props using Array.reduce()
     const props = Array.from(node.attributes).reduce((result, attr) => {
       result[attr.nodeName] = attr.nodeValue;
@@ -12,15 +15,40 @@ function convertNodeToReact(node) {
     // Recursively convert the children of the element to React components
     const children = Array.from(node.childNodes).map(convertNodeToReact);
 
-    // Return the React element
-    return React.createElement(node.tagName.toLowerCase(), props, children);
+    // We gotta check if you are a custom element and have self closing tag
+    const isSelfClosingCustomElement = isCustomSelfClosingElement(node);
+
+    // Handle creating an element with a self closing tag and if you are a custom element
+    // We gotta also handle your children if you have any
+    // We cannot use React.createElement because they don't support self closing tags for custom elements
+    if (isSelfClosingCustomElement) {
+      // TO DO:
+
+    // Return the element with the tag name
+    return React.createElement(tagName, props, ...children);
   } else if (node.nodeType === Node.TEXT_NODE) {
     // Return the text content of the node as a string
     return node.textContent;
   } else {
-    // Ignore comments and other node types
+    // Ignore other node types (for now cause theres only so much time in the day!)
     return null;
   }
+}
+
+// Reference :
+// https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements
+// https://developer.mozilla.org/en-US/docs/Glossary/Void_element
+export function isCustomSelfClosingElement(node) {
+  // Get the tag name
+  const tagName = node.tagName.toLowerCase();
+
+  // Is a void element (self closing) and does not have children
+  const isVoidElement = node.childNodes.length === 0;
+
+  // Check if the element is a custom element
+  const isACustomElement = tagName.includes(':') || tagName.includes('-');
+
+  return isVoidElement && isACustomElement;
 }
 
 export default function stringToReactComponent(html) {
@@ -30,20 +58,22 @@ export default function stringToReactComponent(html) {
     const fragment = document.createRange().createContextualFragment(html);
 
     // Convert the child nodes of the fragment to an array of React elements
-    const reactComponents = Array.from(fragment.childNodes).map(convertNodeToReact);
+    const reactComponents = Array.from(fragment.childNodes).map(
+      convertNodeToReact
+    );
 
     // Join the array of React elements into a single element
-    return (
-      <>{reactComponents}</>
-    );
+    return <>{reactComponents}</>;
   } else {
     // Parse the HTML string into a DOM tree (server-side)
     const { document } = parseHTML(html);
 
     // Convert the child nodes of the document to an array of React elements
-    const reactComponents = Array.from(document.body.childNodes).map(convertNodeToReact).filter(Boolean);
+    const reactComponents = Array.from(document.body.childNodes).map(
+      convertNodeToReact
+    );
 
     // Join the array of React elements into a single element
-    return React.createElement(React.Fragment, null, ...reactComponents);
+    return <>{reactComponents}</>;
   }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import stringToReactComponent from './index';
+import stringToReactComponent, { isCustomSelfClosingElement } from './index';
 
 describe('stringToReactComponent', () => {
   it('renders an empty string as an empty React component', () => {
@@ -17,14 +17,23 @@ describe('stringToReactComponent', () => {
   });
 
   it('renders nested HTML elements as a nested React component', () => {
-    const html = '<div><p>Paragraph <strong>with strong text</strong></p></div>';
+    const html =
+      '<div><p>Paragraph <strong>with strong text</strong></p></div>';
     const { container } = render(stringToReactComponent(html));
     expect(container.firstChild.tagName.toLowerCase()).toBe('div');
     expect(container.firstChild.firstChild.tagName.toLowerCase()).toBe('p');
-    expect(container.firstChild.firstChild.firstChild.nodeType).toBe(Node.TEXT_NODE);
-    expect(container.firstChild.firstChild.firstChild.textContent).toBe('Paragraph ');
-    expect(container.firstChild.firstChild.lastChild.tagName.toLowerCase()).toBe('strong');
-    expect(container.firstChild.firstChild.lastChild.textContent).toBe('with strong text');
+    expect(container.firstChild.firstChild.firstChild.nodeType).toBe(
+      Node.TEXT_NODE
+    );
+    expect(container.firstChild.firstChild.firstChild.textContent).toBe(
+      'Paragraph '
+    );
+    expect(
+      container.firstChild.firstChild.lastChild.tagName.toLowerCase()
+    ).toBe('strong');
+    expect(container.firstChild.firstChild.lastChild.textContent).toBe(
+      'with strong text'
+    );
   });
 
   it('renders HTML attributes as React props', () => {
@@ -42,11 +51,103 @@ describe('stringToReactComponent', () => {
     expect(container.firstChild.textContent).toBe('Text content');
   });
 
-  it('renders HTML entities as text content', () => {
-    const html = '&lt;div&gt;Hello world&lt;/div&gt;';
+  it('renders div as they should be', () => {
+    const html = '<div>Hello world</div>';
     const { container } = render(stringToReactComponent(html));
-    expect(container.firstChild.nodeType).toBe(Node.TEXT_NODE);
-    expect(container.firstChild.textContent).toBe('<div>Hello world</div>');
+    expect(container.firstChild.nodeType).toBe(Node.ELEMENT_NODE);
+    expect(container.firstChild.outerHTML).toBe('<div>Hello world</div>');
   });
+
+  it('renders scripts as they should be', () => {
+    const html = '<script>function(){}</script>';
+    const { container } = render(stringToReactComponent(html));
+    expect(container.firstChild.nodeType).toBe(Node.ELEMENT_NODE);
+    expect(container.firstChild.outerHTML).toBe(
+      '<script>function(){}</script>'
+    );
+  });
+
+  it('renders div with attributes as they should be', () => {
+    const html = '<div id="myDiv" class="myClass">Hello world</div>';
+    const { container } = render(stringToReactComponent(html));
+    expect(container.firstChild.nodeType).toBe(Node.ELEMENT_NODE);
+    expect(container.firstChild.outerHTML).toBe(
+      '<div id="myDiv" class="myClass">Hello world</div>'
+    );
+  });
+
+  it('renders div with nested elements as they should be', () => {
+    const html = '<div><p>Hello</p><p>World</p></div>';
+    const { container } = render(stringToReactComponent(html));
+    expect(container.firstChild.nodeType).toBe(Node.ELEMENT_NODE);
+    expect(container.firstChild.outerHTML).toBe(
+      '<div><p>Hello</p><p>World</p></div>'
+    );
+  });
+
+  it('renders link element with target attribute as they should be', () => {
+    const html =
+      '<link rel="stylesheet" type="text/css" href="style.css" target="_blank"/>';
+    const { container } = render(stringToReactComponent(html));
+    expect(container.firstChild.nodeType).toBe(Node.ELEMENT_NODE);
+    expect(container.firstChild.outerHTML).toBe(
+      '<link rel="stylesheet" type="text/css" href="style.css" target="_blank">'
+    );
+  });
+
+  it('renders image element with attributes as they should be', () => {
+    const html = '<img src="image.jpg" alt="A beautiful sunset"/>';
+    const { container } = render(stringToReactComponent(html));
+    expect(container.firstChild.nodeType).toBe(Node.ELEMENT_NODE);
+    expect(container.firstChild.outerHTML).toBe(
+      '<img src="image.jpg" alt="A beautiful sunset">'
+    );
+  });
+
+  it('renders esi:include', () => {
+    const html = "<esi:include src='foo_bar'/>";
+    const { container } = render(stringToReactComponent(html));
+    expect(container.firstChild.nodeType).toBe(Node.ELEMENT_NODE);
+    expect(container.firstChild.outerHTML).toBe(
+      '<esi:include src="foo_bar"></esi:include>'
+    );
+  });
+
+  // it('renders esi:include with self-closing tag', () => {
+  //   const html = "<esi:include src='foo_bar'/>";
+  //   const { container } = render(stringToReactComponent(html));
+  //   expect(container.firstChild.nodeType).toBe(Node.ELEMENT_NODE);
+  //   expect(container.firstChild.outerHTML).toBe('<esi:include src="foo_bar"/>');
+  // });
 });
 
+describe('isCustomSelfClosingElement', () => {
+  it('should return false for a self-closing built-in element', () => {
+    const node = document.createElement('input');
+    node.setAttribute('type', 'text');
+    const result = isCustomSelfClosingElement(node);
+    expect(result).toBe(false);
+  });
+
+  it('should return false for a non-self-closing custom element', () => {
+    const node = document.createElement('my-element');
+    node.innerHTML = '<span>Test</span>';
+    const result = isCustomSelfClosingElement(node);
+    expect(result).toBe(false);
+  });
+
+  it('should return false for a non-self-closing built-in element', () => {
+    const node = document.createElement('div');
+    node.innerHTML = '<input type="text">';
+    const inputNode = node.querySelector('input');
+    const result = isCustomSelfClosingElement(inputNode);
+    expect(result).toBe(false);
+  });
+
+  it('should return true for a non-self-closing built-in element', () => {
+    const node = document.createElement('esi:include');
+    node.setAttribute('src', 'foo');
+    const result = isCustomSelfClosingElement(node);
+    expect(result).toBe(true);
+  });
+});
